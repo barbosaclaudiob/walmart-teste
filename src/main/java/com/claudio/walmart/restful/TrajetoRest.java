9e1d94a131f3ba88f53d382015cea9f06e5d273e
Claudio Barbosa <barbosa.claudio@outlook.com>
2016-08-18 16:47:42 -0300

    Retirado importações não utilizadas na classe TrajetoRest.java

--- src/main/java/com/claudio/walmart/restful/TrajetoRest.java
+++ src/main/java/com/claudio/walmart/restful/TrajetoRest.java
@@ -1,25 +1,15 @@
 package com.claudio.walmart.restful;
 
-import com.claudio.walmart.model.Mapa;
-import com.claudio.walmart.model.Rota;
 import com.claudio.walmart.model.Trajeto;
 import com.claudio.walmart.service.MapaService;
 import com.claudio.walmart.service.TrajetoService;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -57,7 +47,7 @@ public class TrajetoRest {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public String sayPlainTextHello(String inputJsonObj) throws Exception {
+    public String salvaMapas(String inputJsonObj) throws Exception {
         MapaService ms = new MapaService();
         ms.salvaMapaJson(inputJsonObj);
 
